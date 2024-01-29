f8d6abc00b5ffb73b56b915b6f745317603b6fd0
Thiago Butignon <thiagobutignon@gmail.com>
2024-01-29 13:19:32 -0300

    test: ensure when we have an error it is undefined

--- src/application/service/stub-service-create-booking.spec.ts
+++ src/application/service/stub-service-create-booking.spec.ts
@@ -68,6 +68,7 @@ describe('StubServiceCreateBooking', () => {
 
     const httpResponse = await sut.request(request)
 
-    expect(httpResponse.statusCode).toBe(HttpStatusCode.badRequest) // Handling of internal errors
+    expect(httpResponse.statusCode).toBe(HttpStatusCode.badRequest)
+    expect(httpResponse.body.booking).toBeUndefined()
   })
 })
