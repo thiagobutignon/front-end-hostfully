7b79e41f78825669ff27d2f8248c1210c446044a
Thiago Butignon <thiagobutignon@gmail.com>
2024-01-29 01:04:58 -0300

    test: ensure when the start and end date are equal will return 1 night

--- src/application/calculators/booking-calculator.spec.ts
+++ src/application/calculators/booking-calculator.spec.ts
@@ -19,4 +19,17 @@ describe('BookingCalculator', () => {
     expect(result.totalPrice).toBe('400.00')
     expect(result.numberOfNights).toBe(4)
   })
+
+  it('should correctly calculate the total price for a 1-day booking', () => {
+    const params: BookingCalculateTotalPrice.Params = {
+      startDate: new Date('2024-01-01'),
+      endDate: new Date('2024-01-01'),
+      pricePerNight: '100'
+    }
+
+    const result = sut.execute(params)
+
+    expect(result.totalPrice).toBe('100.00')
+    expect(result.numberOfNights).toBe(1)
+  })
 })
--- src/application/calculators/booking-calculator.ts
+++ src/application/calculators/booking-calculator.ts
@@ -2,12 +2,17 @@ import { BookingCalculateTotalPrice } from '@/application/protocols'
 
 export class BookingCalculator implements BookingCalculateTotalPrice {
   execute (params: BookingCalculateTotalPrice.Params): BookingCalculateTotalPrice.Result {
+    let numberOfNights
     const start = new Date(params.startDate)
     const end = new Date(params.endDate)
     const nightlyRate = parseFloat(params.pricePerNight)
+    if (start.toDateString() === end.toDateString()) {
+      numberOfNights = 1
+    } else {
+      const timeDiff = end.getTime() - start.getTime()
+      numberOfNights = Math.ceil(timeDiff / (1000 * 3600 * 24))
+    }
 
-    const timeDiff = end.getTime() - start.getTime()
-    const numberOfNights = Math.ceil(timeDiff / (1000 * 3600 * 24))
     const totalPrice = (numberOfNights * nightlyRate).toFixed(2).toString()
     return {
       numberOfNights,
