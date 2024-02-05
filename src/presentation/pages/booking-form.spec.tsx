2fcae95447ca28e7d07e791e92c46d2200cbf2b9
Thiago Butignon <thiagobutignon@gmail.com>
2024-02-05 19:39:46 -0300

    test: add tests to ensure the button is called

--- src/presentation/pages/booking-form.spec.tsx
+++ src/presentation/pages/booking-form.spec.tsx
@@ -1,9 +1,9 @@
 import { CreateBookingUsecase, ListBookingsUsecase } from '@/domain/usecases'
+import { act, fireEvent, render } from '@testing-library/react'
 
 import BookingForm from '@/presentation/pages/booking-form'
 import { Validation } from '@/validation/protocols'
 import { ChakraProvider } from '@chakra-ui/react'
-import { render } from '@testing-library/react'
 import { clear } from 'console'
 
 const mockedDate = new Date('2024-02-05')
@@ -73,6 +73,31 @@ describe('BookingForm', () => {
     )
 
     expect(getByTestId('form')).toBeInTheDocument()
+  })
+
+  it('submits form with correct data', async () => {
+    const { getByTestId, getByLabelText } = render(
+      <BookingForm
+        reloadFlag={false}
+        listBookings={mockListBookings}
+        validation={mockValidation}
+        createBooking={mockCreateBooking}
+        onBookingSubmitted={onBookingSubmitted}
+      />
+    )
+
+    await act(async () => {
+      fireEvent.change(getByTestId('guestEmail'), {
+        target: { value: 'test@example.com' }
+      })
+      fireEvent.change(getByLabelText('Number of Guests:'), {
+        target: { value: '1' }
+      })
+
+      fireEvent.submit(getByTestId('form'))
+    })
+
+    expect(mockCreateBooking.perform).toHaveBeenCalledTimes(1)
   })
 
   it('matches snapshot', () => {
